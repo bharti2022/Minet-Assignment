646f634cb5bb9eac9649e913a474a29a6f8b4350
bharti2022 <bharti.sagar@zemosolabs.com>
2022-07-11 12:14:55 +0530

    components added

---
+++ src/components/organisms/USDWalletGrid/USDWalletGrid.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const USDWalletGrid = () => {
+  return (
+    <div>USDWalletGrid</div>
+  )
+}
+
+export default USDWalletGrid
---
+++ src/components/organisms/WatchlistBar/WatchlistBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WatchlistBar = () => {
+  return (
+    <div>WatchlistBar</div>
+  )
+}
+
+export default WatchlistBar
